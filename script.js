0d648084caf5e47d4d559599761f17012ffbd27a
L kabeer <ahmed@Lkabeer.com>
2017-11-28 10:40:43 +0200

    version 2

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// Initialize Firebase
-var config = {
-apiKey: "AIzaSyDD-NMt3W8NYl9ZnUI5rFBGy3ZWpa9TuSo",
-authDomain: "fir-database-ecca0.firebaseapp.com",
-databaseURL: "https://fir-database-ecca0.firebaseio.com",
-projectId: "fir-database-ecca0",
-storageBucket: "",
-messagingSenderId: "987339674979"
-};
-firebase.initializeApp(config);
-
-var textX = document.getElementById('testX');
-var dbRef = firebase.database().ref().child('text');
-dbRef.on('value', snap => textX.innerText = snap.val());
+(function() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDD-NMt3W8NYl9ZnUI5rFBGy3ZWpa9TuSo",
+        authDomain: "fir-database-ecca0.firebaseapp.com",
+        databaseURL: "https://fir-database-ecca0.firebaseio.com",
+        projectId: "fir-database-ecca0",
+        storageBucket: "",
+        messagingSenderId: "987339674979"
+    };
+    firebase.initializeApp(config);
+    
+    // Get elements
+    const preObject = document.getElementById('object');
+    
+    // Create references
+    const dbRefObject = firebase.database().ref().child('object');
+    
+    // Sync object changes
+    // dbRefObject.on('value', snap => console.log(snap.val()));
+    dbRefObject.on('value', snap => {
+        preObject.innerText = JSON.stringify(snap.val(), null, 3);
+    });
+    
+}());
