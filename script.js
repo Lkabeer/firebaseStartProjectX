f89f047c6d459b5292755bea077a78e34cf45bd0
L kabeer <ahmed@Lkabeer.com>
2017-11-28 13:26:11 +0200

    version 3.1

--- script.js
+++ script.js
@@ -17,14 +17,17 @@
     // // version 1 X-Team end
     
 
-    // version 2 X-Team Started
+    // version 2 X-Team Start
     // Get elements
     const preObject = document.getElementById('object');
     const ulList = document.getElementById('list');
+    const ulFriendy = document.getElementById('friendy');
     
     // Create references
     const dbRefObject = firebase.database().ref().child('object');
     const dbRefList = dbRefObject.child('hobbies');
+
+    const dbRefNotifications = dbRefObject.child('notifications');
     
     // Sync object changes
     // dbRefObject.on('value', snap => console.log(snap.val()));
@@ -50,5 +53,39 @@
         liToRemove.remove();
     });
     // version 2 X-Team end
+
+
+    // version 3 X-Team Started
+
+    // Sync object changes
+    timeAgo = [];
+    dbRefNotifications.on('child_added', snap => {
+        const liContent = document.createElement('li');
+        liContent.innerText = "Content: " + snap.child('content').val();
+        liContent.id = snap.key;
+        ulFriendy.appendChild(liContent);
+
+        const liTitle = document.createElement('li');
+        liTitle.innerText = "Title: " + snap.child('title').val();
+        liTitle.id = snap.key;
+        ulFriendy.appendChild(liTitle);
+
+        const liNumber = document.createElement('li');
+        liNumber.innerText = "Number: " + snap.child('number').val();
+        liNumber.id = snap.key;
+        ulFriendy.appendChild(liNumber);
+        
+        const timeAgo = document.createElement('li');
+        var newNumber = snap.child('number').val() * 2;        
+        timeAgo.innerText = "Time Ago: " + newNumber;
+        timeAgo.id = snap.key;
+        ulFriendy.appendChild(timeAgo);
+
+        var newNumber = snap.child('number').val();
+        console.log(newNumber * 2);
+    });
+
+    // version 3 X-Team end
+
     
 }());
