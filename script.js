b2cfff4c9f39904bec801f0f1ff16d134966ad4b
L kabeer <ahmed@Lkabeer.com>
2017-11-28 12:04:18 +0200

    version 3

--- script.js
+++ script.js
@@ -9,17 +9,46 @@
         messagingSenderId: "987339674979"
     };
     firebase.initializeApp(config);
+
+    // // version 1 X-Team Started
+    //     const textX = document.getElementById('testX');
+    //     const dbRef = firebase.database().ref().child('text');
+    //     dbRef.on('value', snap => textX.innerText = snap.val());
+    // // version 1 X-Team end
     
+
+    // version 2 X-Team Started
     // Get elements
     const preObject = document.getElementById('object');
+    const ulList = document.getElementById('list');
     
     // Create references
     const dbRefObject = firebase.database().ref().child('object');
+    const dbRefList = dbRefObject.child('hobbies');
     
     // Sync object changes
     // dbRefObject.on('value', snap => console.log(snap.val()));
     dbRefObject.on('value', snap => {
         preObject.innerText = JSON.stringify(snap.val(), null, 3);
     });
+
+    // Sync list changes 
+    dbRefList.on('child_added', snap => {
+        const li = document.createElement('li');
+        li.innerText = snap.val();
+        li.id = snap.key;
+        ulList.appendChild(li);
+    });
+
+    dbRefList.on('child_changed', snap => {
+        const liChanged = document.getElementById(snap.key);
+        liChanged.innerText = snap.val(); 
+    });
+
+    dbRefList.on('child_removed', snap => {
+        const liToRemove = document.getElementById(snap.key);
+        liToRemove.remove();
+    });
+    // version 2 X-Team end
     
 }());
